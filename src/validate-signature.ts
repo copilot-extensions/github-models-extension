45545f0e697a357d88939da3ed695b2f2dddab29
Jason Etcovitch <jasonetco@github.com>
2024-08-26 21:20:23 -0400

    Need to await promise

--- src/validate-signature.ts
+++ src/validate-signature.ts
@@ -1,14 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { verify } from "@copilot-extensions/preview-sdk";
 
-interface GitHubKeysPayload {
-  public_keys: Array<{
-    key: string;
-    key_identifier: string;
-    is_current: boolean;
-  }>;
-}
-
 export async function verifySignatureMiddleware(
   req: Request,
   res: Response,
@@ -19,7 +11,10 @@ export async function verifySignatureMiddleware(
       const signature = req.get("GitHub-Public-Key-Signature") as string;
       const keyID = req.get("GitHub-Public-Key-Identifier") as string;
       const tokenForUser = req.get("X-GitHub-Token") as string;
-      if (!verify(req.body, signature, keyID, { token: tokenForUser })) {
+      const verified = await verify(req.body, signature, keyID, {
+        token: tokenForUser,
+      });
+      if (!verified) {
         console.log("Signature verification failed");
         return res.status(401).send("Unauthorized");
       }
