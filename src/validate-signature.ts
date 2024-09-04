21ffac3794ab1c9d255941562711f0001723ed0b
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2024-09-04 14:44:35 -0700

    refactor: adapt for `@copilot-extensions/preview-sdk` v2

--- src/validate-signature.ts
+++ src/validate-signature.ts
@@ -1,5 +1,5 @@
 import express, { type Request, Response, NextFunction } from "express";
-import { verify } from "@copilot-extensions/preview-sdk";
+import { verifyAndParseRequest } from "@copilot-extensions/preview-sdk";
 
 export async function verifySignatureMiddleware(
   req: Request,
@@ -11,17 +11,17 @@ export async function verifySignatureMiddleware(
       const signature = req.get("GitHub-Public-Key-Signature") as string;
       const keyID = req.get("GitHub-Public-Key-Identifier") as string;
       const tokenForUser = req.get("X-GitHub-Token") as string;
-      const verified = await verify(req.body, signature, keyID, {
+      const { isValidRequest, payload } = await verifyAndParseRequest(req.body, signature, keyID, {
         token: tokenForUser,
       });
-      if (!verified) {
+      if (!isValidRequest) {
         console.log("Signature verification failed");
         return res.status(401).send("Unauthorized");
       }
 
       console.log("Signature verified");
 
-      req.body = JSON.parse(req.body.toString("utf-8"));
+      req.body = payload
       next();
     } catch (err) {
       console.error(err);
